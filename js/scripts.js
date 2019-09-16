68f5029afc3c1a9b5c444b342f9ed1d0f8386bcc
Barath <barath6.99@gmail.com>
2019-09-16 20:09:15 +0530

    npm scripts changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
-        $(document).ready(function(){
-            $("#mycarousel").carousel( { interval: 2000 } );
-            $("#carouselButton").click(function(){
-                if($("#carouselButton").children("span").hasClass('fa-pause'))
-                {
-                    $("#mycarousel").carousel('pause');
-                    $("#carouselButton").children("span").removeClass('fa-pause');
-                    $("#carouselButton").children("span").addClass("fa-play");
-                }
-                else if($("#carouselButton").children("span").hasClass("fa-play"))
-                {
-                    $("#mycarousel").carousel("cycle");
-                    $("#carouselButton").children("span").removeClass("fa-play");
-                    $("#carouselButton").children("span").addClass("fa-pause");
-                }
-            });
-            $('#loginbtn').click(function(){
-                console.log("hi");
-                $("#loginModal").modal();
-            })
-            $('#reservebtn').click(function(){
-                console.log("hi");
-                $("#resevemodal").modal();
-            })
-        });
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if($("#carouselButton").children("span").hasClass('fa-pause'))
+        {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass("fa-play");
+        }
+        else if($("#carouselButton").children("span").hasClass("fa-play"))
+        {
+            $("#mycarousel").carousel("cycle");
+            $("#carouselButton").children("span").removeClass("fa-play");
+            $("#carouselButton").children("span").addClass("fa-pause");
+        }
+    });
+    $('#loginbtn').click(function(){
+        console.log("hi");
+        $("#loginModal").modal();
+    })
+    $('#reservebtn').click(function(){
+        console.log("hi");
+        $("#resevemodal").modal();
+    })
+});
